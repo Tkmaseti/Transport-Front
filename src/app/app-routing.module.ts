51cc110af4a7e559bd794c208760d849c95c672d
Tkmaseti <maseti.tsepang@gmail.com>
2023-06-07 16:26:10 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './_components/home/home.component';
+import { AboutComponent } from './_components/about/about.component';
 
 const routes: Routes = [
-  {path: "", component:HomeComponent}
+  {path: "", component:HomeComponent},
+  {path:"about", component:AboutComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { HomeComponent } from './_components/home/home.component';
+import { AboutComponent } from './_components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
